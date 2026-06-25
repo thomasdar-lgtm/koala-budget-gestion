f264f30d917a72a38a4c9c57f2802b7432629ddf
thomasdar-lgtm <thomas.darregert@gmail.com>
2026-06-25 17:05:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kbg-v14';
+const CACHE_NAME = 'kbg-v15';
 const ASSETS = [
   '/koala-budget-gestion/',
   '/koala-budget-gestion/index.html',
@@ -6,25 +6,16 @@ const ASSETS = [
   '/koala-budget-gestion/icons/icon-192.png',
   '/koala-budget-gestion/icons/icon-512.png'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
