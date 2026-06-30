6d6fab7586a669b4ba3f7d2fd0ee12b4b26031b1
thomasdar-lgtm <thomas.darregert@gmail.com>
2026-06-30 19:48:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kbg-v46';
+const CACHE_NAME = 'kbg-v47';
 const ASSETS = ['/koala-budget-gestion/','/koala-budget-gestion/index.html','/koala-budget-gestion/manifest.json','/koala-budget-gestion/icons/icon-192.png','/koala-budget-gestion/icons/icon-512.png'];
 self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));self.clients.claim();});
